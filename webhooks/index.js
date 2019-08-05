f1f4132812ec0072baebafdf3611299eaa4170ec
RikkaW <rikka@shizuku.moe>
2019-08-05 23:18:22 +0800

    only rebuild updated

--- webhooks/index.js
+++ webhooks/index.js
@@ -9,13 +9,38 @@ const crypto = require('crypto')
 const {
     spawn
 } = require('child_process')
+const fs = require('fs')
 
-http.createServer(function(req, res) {
+function markBuild(body) {
+    let folders = new Set()
+    body.commits.forEach(commit => {
+        let changes = commit.added.concat(commit.removed).concat(commit.modified)
+        changes.forEach(function (change, index, array) {
+            let i = change.indexOf('/')
+            if (i != -1) {
+                let name = change.substring(0, i)
+                folders.add(name)
+            }
+        })
+
+        new Set(folders).forEach(function (key, value, set) {
+            if (!fs.lstatSync(value).isDirectory() || value == 'webhooks')
+                folders.delete(value)
+        })
+    })
+
+    folders.forEach(function (key, value, set) {
+        fs.closeSync(fs.openSync(value + '/.rebuild', 'w'))
+    })
+    return folders
+}
+
+http.createServer(function (req, res) {
     let body = []
 
     req.on('error', (err) => {
         console.error(err)
-    }).on('data', function(data) {
+    }).on('data', function (data) {
         body.push(data)
     }).on('end', () => {
         body = Buffer.concat(body).toString()
@@ -27,17 +52,31 @@ http.createServer(function(req, res) {
             res.end()
             return
         }
+        
+        let folders
+        try {
+            folders = markBuild(JSON.parse(body))
+        } catch (e) {
+            console.log(e)
+            res.write(e.toString())
+            res.write('\n')
+        }
+        folders.forEach(function (key, value, set) {
+            console.log("Folder " + value + " requires rebuild")
+            res.write("Folder " + value + " requires rebuild.\n")
+        })
 
+        console.log("Calling git pull")
         let child = spawn('git', ['pull'])
-        child.stdout.on('data', function(data) {
+        child.stdout.on('data', function (data) {
             res.write(data)
         })
-        child.stderr.on('data', function(data) {
+        child.stderr.on('data', function (data) {
             res.write(data)
         })
         child.on('close', (code) => {
             res.write(`\nChild process exited with code ${code}`)
-			
+
             res.statusCode = 200
             res.end()
         })
